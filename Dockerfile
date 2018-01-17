e409b8ebee073e00c0551097343d7dac033ba94e
Christian Kniep <christian@qnib.org>
2018-01-17 21:16:42 +0100

    clustered setup

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM qnib/dplain-init
+FROM qnib/uplain-init
 
 ARG DGRAPH_URL=https://github.com/dgraph-io/dgraph
 ARG DGRAPH_VER=v1.0.1
 RUN apt-get update \
- && apt-get install -y wget \
+ && apt-get install -y wget iproute2 curl jq \
  && wget -qO - \
     ${DGRAPH_URL}/releases/download/${DGRAPH_VER}/dgraph-linux-amd64-${DGRAPH_VER}.tar.gz \
     |tar xfz - -C /usr/bin/
--- bin/dgraph.sh
+++ bin/dgraph.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+if [[ "X${DGRAPH_IDX}" == "X" ]];then
+    DGRAPH_IDX=1
+fi
+
 ZERO_ADDR=tasks.zero
 if [[ "X${2}" != "X" ]];then
   ZERO_ADDR=${2}
@@ -10,7 +14,7 @@ MY_IP=$(ip -o -4 addr |awk '/eth0/{print $4}' |awk -F/ '{print $1}')
 if [[ "${1}" == "zero" ]];then
   dgraph zero --bindall=true --my=${MY_IP}:7080
 elif [[ "${1}" == "server" ]]; then
-  dgraph server --port_offset 1 --memory_mb=1024 --zero=${ZERO_ADDR}:7080 --my=${MY_IP}:7081 --idx 01234
+  dgraph server --port_offset 1 --memory_mb=1024 --zero=${ZERO_ADDR}:7080 --my=${MY_IP}:7081 --idx ${DGRAPH_IDX}
 elif [[ "${1}" == "ratel" ]]; then
   dgraph-ratel -p 8082
 fi
